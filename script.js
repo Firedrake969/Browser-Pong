837f78adc7a02bbe7cc4943565381255610fda13
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-07-15 15:33:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- /***********************************************************************
+/***********************************************************************
 
 BROWSER PONG
 ===========================
@@ -15,102 +15,114 @@ var scorePlayer = 0;
 var scoreOpponent = 0;
 var cpuSpeed = 0;
 var randOffset = 15;
+
+function ballBounce(condition) {
+    if (condition) {
+        ball.css("left", Math.round(ball.position().left + Math.sin(ballDir) * (0 - dist)) + "px");
+
+        if (condition) {
+            ballDir = ((Math.floor(Math.random() * ((2 * randOffset) + 1)) - randOffset) + (180 - ((180 / Math.PI) * ballDir))) * (Math.PI / 180);
+        } else {
+            ballDir = ((Math.floor(Math.random() * ((2 * randOffset) + 1)) - randOffset) + (0 - ((180 / Math.PI) * ballDir))) * (Math.PI / 180);
+        }
+        ball.css("left", Math.round(ball.position().left + Math.sin(ballDir) * dist) + "px");
+    }
+}
+
+function aiMove() {
+    if (ball.position().left > cpu.position().left + 30) {
+        if (ball.position().left - cpu.position().left + 30 < 120) {
+            if (ball.position().top - cpu.position().top < 120) {
+                cpuSpeed = 2;
+            } else {
+                cpuSpeed = 2 - (Math.random() / 2); //from 1.5-2
+            }
+        } else {
+            if (ball.position().top - cpu.position().top > 250) {
+                cpuSpeed = 1;
+            } else if (ball.position().top - cpu.position().top > 160) {
+                cpuSpeed = 1.5;
+            } else {
+                cpuSpeed = 2;
+            }
+        }
+    } else {
+        if (cpu.position().left - ball.position().left + 30 < 120) {
+            if (ball.position().top - cpu.position().top < 120) {
+                cpuSpeed = -2;
+            } else {
+                cpuSpeed = -2 + (Math.random() / 2);
+            }
+        } else {
+            if (ball.position().top - cpu.position().top > 250) {
+                cpuSpeed = -1;
+            } else if (ball.position().top - cpu.position().top > 160) {
+                cpuSpeed = -1.5;
+            } else {
+                cpuSpeed = -2;
+            }
+        }
+    }
+    cpu.css("left", (cpu.position().left + cpuSpeed) + "px");
+    if (cpu.position().left < 5) {
+        cpu.css("left", "5px");
+    }
+    if (cpu.position().left > 440) {
+        cpu.css("left", "435px");
+    }
+}
+
+function reset() {
+    ball.css('top', 190 + 'px');
+    ball.css('left', 250 + 'px');
+    ballDir = (Math.floor(Math.random() * (360) + 1)) * (Math.PI / 180);
+    dist = 2;
+}
+
+function detectGoal() {
+    if (ball.position().top < 15) {
+        scorePlayer += 1;
+        $("#playerScore").text(scorePlayer);
+        reset();
+    }
+    if (ball.position().top > 380) {
+        scoreOpponent += 1;
+        $("#cpuScore").text(scoreOpponent);
+        reset();
+    }
+}
+
+function ballMove() {
+    ball.css("top", Math.round(ball.position().top - (dist * Math.sin(ballDir))) + 'px');
+    ball.css("left", Math.round(ball.position().left + (dist * Math.cos(ballDir))) + 'px');
+}
+
+function paddleBounce(condition) {
+    if (condition) {
+        ballDir = (180 - (ballDir * (180 / Math.PI))) * (Math.PI / 180);
+    }
+}
+
 $(document).keydown(function (e) {
-    if (e.keyCode === 68 && player.position().left < 215) {
+    if (e.keyCode === 68 && player.position().left < 430) {
         player.css("left", player.position().left + playerSpeed + 'px');
-    } else if (e.keyCode === 65 && player.position().left > -215) {
+    } else if (e.keyCode === 65 && player.position().left > 10) {
         player.css("left", player.position().left - playerSpeed + 'px');
     }
 });
+
 $(document).mouseenter(function () {
     $("#ball").click(function () {
         if (starting) {
             setInterval(function () {
-                //Function for the ball's bounce
-                function ballBounce(condition) {
-                    if (condition) {
-                        ball.css("left", Math.round(ball.position().left + Math.sin(ballDir) * (0 - dist)) + "px");
-
-                        if (condition) {
-                            ballDir = ((Math.floor(Math.random() * ((2 * randOffset) + 1)) - randOffset) + (180 - ((180 / Math.PI) * ballDir))) * (Math.PI / 180);
-                        } else {
-                            ballDir = ((Math.floor(Math.random() * ((2 * randOffset) + 1)) - randOffset) + (0 - ((180 / Math.PI) * ballDir))) * (Math.PI / 180);
-                        }
-                        ball.css("left", Math.round(ball.position().left + Math.sin(ballDir) * dist) + "px");
-                    }
-                }
-                //Ball actual motion
-                ball.css("top", Math.round(ball.position().top + (dist * Math.sin(ballDir))) + 'px');
-                ball.css("left", Math.round(ball.position().left + (dist * Math.cos(ballDir))) + 'px');
+                ballMove();
                 //Ball bouncing!  Will be combined later.
                 //Check for walls
                 ballBounce(ball.position().left < 0 || ball.position().left > 500);
                 //Check for the player paddle
-                ballBounce(ball.position().left < player.position().left + 70 && ball.position().left + 10 > player.position().left && ball.position().top < 380 && ball.position().top > 370);
-                //AI paddle
-                ballBounce(ball.position().left < cpu.position().left + 70 && ball.position().left + 10 > cpu.position().left && ball.position().top < 24 && ball.position().top > 26);
+                paddleBounce(ball.position().left < player.position().left + 70 && ball.position().left + 10 > player.position().left && ball.position().top > 350);
                 //AI MOTION
-                function aiMove() {
-                    if (ball.position().left > cpu.position().left + 30) {
-                        if (ball.position().left - cpu.position().left + 30 < 120) {
-                            if (ball.position().top - cpu.position().top < 120) {
-                                cpuSpeed = 2;
-                            } else {
-                                cpuSpeed = 2 - (Math.random() / 2); //from 1.5-2
-                            }
-                        } else {
-                            if (ball.position().top - cpu.position().top > 250) {
-                                cpuSpeed = 1;
-                            } else if (ball.position().top - cpu.position().top > 160) {
-                                cpuSpeed = 1.5;
-                            } else {
-                                cpuSpeed = 2;
-                            }
-                        }
-                    } else {
-                        if (cpu.position().left - ball.position().left + 30 < 120) {
-                            if (ball.position().top - cpu.position().top < 120) {
-                                cpuSpeed = -2;
-                            } else {
-                                cpuSpeed = -2 + (Math.random() / 2);
-                            }
-                        } else {
-                            if (ball.position().top - cpu.position().top > 250) {
-                                cpuSpeed = -1;
-                            } else if (ball.position().top - cpu.position().top > 160) {
-                                cpuSpeed = -1.5;
-                            } else {
-                                cpuSpeed = -2;
-                            }
-                        }
-                    }
-                    cpu.css("left", (cpu.position().left + cpuSpeed) + "px");
-                    if (cpu.position().left < 5) {
-                        cpu.css("left", "5px");
-                    }
-                    if (cpu.position().left > 440) {
-                        cpu.css("left", "435px");
-                    }
-                }
                 aiMove();
-                function reset() {
-                    ball.css('top', 190 + 'px');
-                    ball.css('left', 250 + 'px');
-                    ballDir = (Math.floor(Math.random() * (360) + 1)) * (Math.PI / 180);
-                    dist = 2;
-                }
-                function detectGoal() {
-                    if (ball.position().top < 15) {
-                        scorePlayer += 1;
-                        $("#playerScore").text(scorePlayer);
-                        reset();
-                    }
-                    if (ball.position().top > 375) {
-                        scoreOpponent += 1;
-                        $("#cpuScore").text(scoreOpponent);
-                        reset();
-                    }
-                }
                 detectGoal();
             }, 5);
         }
